c5dea7ef86870fd0f603b4688eda25239e2e0a04
Igor <iny5051@gmail.com>
2025-07-14 18:48:26 -0300

    Adicionando o vimtex

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,7 +4,17 @@ return {
     {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
+    {
+        "lervag/vimtex",
+        lazy = false,     -- we don't want to lazy load VimTeX
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+        end
+    },
+    {
+        'hrsh7th/nvim-cmp',
+    },
     {'L3MON4D3/LuaSnip'},
     {'nvim-telescope/telescope.nvim'},
     {'nvim-treesitter/nvim-treesitter'},
@@ -25,4 +35,16 @@ return {
             {'<leader>sf', "<cmd>Telescope find_files<cr>", desc = "Find file"},
         },
     },
+    {
+        'goolord/alpha-nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            local startify = require("alpha.themes.startify")
+            startify.file_icons.provider = "devicons"
+            require("alpha").setup(
+                startify.config
+            )
+            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        end
+    };
 }
